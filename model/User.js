bec4f30d41f1baabe740d0d6f591a499e4629110
sheandean  git config --global user.email shandeanhutchinson@yahoo.comgit config --global user.name sheandean <shandeanhutchinson@yahoo.com>
2022-03-10 13:59:03 -0500

    removed file from backend folder

--- model/User.js
+++ model/User.js
@@ -3,16 +3,11 @@ const mongoose = require('mongoose');
 //a plugin which adds pre-save validation for unique fields within a Mongoose schema.
 const uniqueValidator = require('mongoose-unique-validator');
 
-// Fucntion to require email validation before signup
-let validateEmail = (email) => {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email);
-};
 
 // MODEL
 const userSchema = mongoose.Schema({
-    userId: { type: String, required: false, unique: true},
-    email: { type: String, required: true, unique: true, validate: [validateEmail, 'Please fill a valid email address'] },
+    
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
 
